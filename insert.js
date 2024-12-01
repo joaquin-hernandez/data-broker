a801918ebb0ef5b450defbd3d4c233f0749d7078
joaquin-hernandez <39408746+joaquin-hernandez@users.noreply.github.com>
2024-11-30 21:21:02 -0300

    emprolijado

--- insert.js
+++ insert.js
@@ -1,18 +1,12 @@
-//import * as dataMapping from './dataMapping.json';
-// import * as data from './dataFile.json';
 var data = require('./dataFile.json');
 var dataMapping = require('./dataMapping.json');
 var formId = "CancelacionHipoteca";
-// Empezamos recorriendo el Mapping y creamos dos arrays
-// El primero con las tablas y el segundo con las objetos/atributos
-//console.log ("vamos con el mapping");
 var table;
 var field;
 var sqlInsertL1;
 var sqlInsertL2;
 for (table in dataMapping.column_field_mapping) {
     sqlInsertL1 = "INSERT INTO " + dataMapping.column_field_mapping[table].table_name + " ";
-    //console.log("LONNGITUD" + dataMapping.column_field_mapping[table].fields);
     // buscamos el ultimo fiels (RE CHOTO)
     var lastField = void 0;
     for (field in dataMapping.column_field_mapping[table].fields) {
@@ -25,7 +19,6 @@ for (table in dataMapping.column_field_mapping) {
         sqlInsertL1 += (dataMapping.column_field_mapping[table].fields[field].column);
         sqlInsertL2 += data.formData[dataMapping.column_field_mapping[table].object_name][dataMapping.column_field_mapping[table].fields[field].field];
         if (lastField != field) {
-            //console.log("AAACCCAA");
             sqlInsertL1 += ", ";
             sqlInsertL2 += ", ";
         }
--- insert.ts
+++ insert.ts
@@ -1,12 +1,7 @@
-//import * as dataMapping from './dataMapping.json';
-// import * as data from './dataFile.json';
 const data = require('./dataFile.json');
 const dataMapping = require('./dataMapping.json');
 const formId="CancelacionHipoteca";
 
-// Empezamos recorriendo el Mapping y creamos dos arrays
-// El primero con las tablas y el segundo con las objetos/atributos
-//console.log ("vamos con el mapping");
 let table:any;
 let field:any;
 let sqlInsertL1:string;
@@ -14,7 +9,6 @@ let sqlInsertL2:string;
 
 for (table in dataMapping.column_field_mapping){
     sqlInsertL1 = "INSERT INTO " + dataMapping.column_field_mapping[table].table_name + " ";
-    //console.log("LONNGITUD" + dataMapping.column_field_mapping[table].fields);
 
     // buscamos el ultimo fiels (RE CHOTO)
     let lastField
@@ -31,11 +25,9 @@ for (table in dataMapping.column_field_mapping){
         sqlInsertL2 += data.formData[dataMapping.column_field_mapping[table].object_name][dataMapping.column_field_mapping[table].fields[field].field];
 
         if(lastField!=field){
-            //console.log("AAACCCAA");
             sqlInsertL1 += ", ";
             sqlInsertL2 += ", ";
         }
-        
     }
     sqlInsertL1 += ")" 
     sqlInsertL2 += ");";
